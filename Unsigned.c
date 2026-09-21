d6db2f88d8137752977e494547cad691c351d661
cangareijo <102051174+cangareijo@users.noreply.github.com>
2026-09-21 17:36:01 +0100

    Update Unsigned.c

--- Unsigned.c
+++ Unsigned.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void print_permutations(const unsigned n) {
+void print_permutations_heaps(const unsigned n) {
   if (n == 0) return;
   unsigned state[n] = {};
   unsigned permutation[n];
@@ -24,7 +24,35 @@ void print_permutations(const unsigned n) {
     }
 }
 
+void print_permutations_lexicographical(const unsigned n) {
+  if (n == 0) return;
+  unsigned permutation[n];
+  for (unsigned i = 0u; i < n; i++) permutation[i] = i;
+  while (true) {
+    for (unsigned i = 0u; i < n; i++) printf("%u ", permutation[i]);
+    printf("\n");
+    unsigned pivot = n - 1u;
+    while (pivot > 0u && permutation[pivot - 1u] >= permutation[pivot]) pivot--;
+    if (pivot == 0u) break;
+    pivot--;
+    unsigned successor = n - 1u;
+    while (permutation[pivot] >= permutation[successor]) successor--;
+    const unsigned temporary = permutation[pivot];
+    permutation[pivot] = permutation[successor];
+    permutation[successor] = temporary;
+    unsigned left = pivot + 1u;
+    unsigned right = n - 1u;
+    while (left < right) {
+      const unsigned reverse = permutation[left];
+      permutation[left] = permutation[right];
+      permutation[right] = reverse;
+      left++;
+      right--;
+    }
+  }
+}
+
 int main() {
-  print_permutations(4);
+  print_permutations_lexicographical(4);
   return 0;
 }
